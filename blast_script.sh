8caab49307f776cc2229e4c118bbd226c8e9077d
Randy Klabacka <u0612750@lp030.lonepeak.peaks>
2023-06-19 12:21:36 -0600

    added comments about moving to scratch

--- blast_script.sh
+++ blast_script.sh
@@ -5,9 +5,11 @@
 bash get_refs.sh -d ref_list_genbank.txt -l refs
 
 #make blast db for each genome
+## move to scratch directory for databases
 for genome in refs; do makeblastdb -in $genome -dbtype nucl -out $genome.db
 done
 
 #blast each genome for fr10
+## move to scratch directory for blast outputs
 for db in dbs; do blastn -query fr10.fasta -db $db -evalue 0.001 -outfmt 6 -out $db.results.txt
 done

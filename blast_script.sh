19c8886fa468624c7bf85b6a2598fc0a235a5f89
reaganman <126733925+reaganman@users.noreply.github.com>
2023-06-30 15:50:23 -0600

    Update blast_script.sh

--- blast_script.sh
+++ blast_script.sh
@@ -1,15 +1,13 @@
-##Need to download fr10.fasta
-##Need to make a directory for all the dbs
-
-#download the genomes 
+#download the genomes
+cd /scratch/general/nfs1/utu_4310/fr10_evolution_wd
 bash get_refs.sh -d ref_list_genbank.txt -l refs
 
 #make blast db for each genome
-## move to scratch directory for databases
-for genome in refs; do makeblastdb -in $genome -dbtype nucl -out $genome.db
+cd /scratch/general/nfs1/utu_4310/fr10_evolution_wd/blast_db
+for genome in /scratch/general/nfs1/utu_4310/fr10_evolution_wd/refs; do makeblastdb -in $genome -dbtype nucl -out $genome.db
 done
 
 #blast each genome for fr10
-## move to scratch directory for blast outputs
-for db in dbs; do blastn -query fr10.fasta -db $db -evalue 0.001 -outfmt 6 -out $db.results.txt
+cd /scratch/general/nfs1/utu_4310/fr10_evolution_wd/blast_outputs
+for db in /scratch/general/nfs1/utu_4310/fr10_evolution_wd/blast_db; do blastn -query fr10.fasta -db $db -evalue 0.001 -outfmt 6 -out $db.results.txt
 done

edd4eb94aa4d174da394ed8d9c3602f94c026bb6
Reagan Mckee <u6052680@lonepeak1.wasatch.peaks>
2023-07-26 11:02:40 -0600

    edit blast script and add fixed ref list

--- blast_script.sh
+++ blast_script.sh
@@ -4,14 +4,14 @@
 #download the genomes
 module load blast/2.11.0
 cd ~/fr10_evolution
-bash /uufs/chpc.utah.edu/common/home/u6052680/fr10_evolution/get_refs.sh -d /scratch/general/nfs1/utu_4310/fr10_evolution_wd/reference_genomes -l ref_list_genbank.txt 
+bash /uufs/chpc.utah.edu/common/home/u6052680/fr10_evolution/get_refs.sh -d /scratch/general/nfs1/utu_4310/fr10_evolution_wd/reference_genomes -l fixed_ref_genbank.txt 
 
 #make blast db for each genome
 cd /scratch/general/nfs1/utu_4310/fr10_evolution_wd/blast_db
-for genome in /uufs/chpc.utah.edu/common/home/u6052680/fr10_evolution/refs; do makeblastdb -in $genome -dbtype nucl -out $genome.db
+for genome in /scratch/general/nfs1/utu_4310/fr10_evolution_wd/reference_genomes/*; do makeblastdb -in $genome -dbtype nucl -out $genome.db
 done
 
 #blast each genome for fr10
 cd /scratch/general/nfs1/utu_4310/fr10_evolution_wd/blast_outputs
-for db in /scratch/general/nfs1/utu_4310/fr10_evolution_wd/blast_db; do blastn -query fr10.fasta -db $db -evalue 0.001 -outfmt 6 -out $db.results.txt
+for db in /scratch/general/nfs1/utu_4310/fr10_evolution_wd/blast_db/*; do blastn -query  /uufs/chpc.utah.edu/common/home/u6052680/fr10_evolution/fr10.fasta -db $db -evalue 0.001 -outfmt 6 -out $db.results.txt
 done

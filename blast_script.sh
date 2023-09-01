76ef3ede7db212075e4b7afe07f733ccb0c41ea6
Reagan Mckee <u6052680@lonepeak1.wasatch.peaks>
2023-09-01 10:21:58 -0600

    add edited script and results.txt

--- blast_script.sh
+++ blast_script.sh
@@ -5,8 +5,9 @@
 module load blast/2.11.0
 cd ~/fr10_evolution
 bash /uufs/chpc.utah.edu/common/home/u6052680/fr10_evolution/get_refs.sh -d /scratch/general/nfs1/utu_4310/fr10_evolution_wd/reference_genomes -l fixed_ref_genbank.txt 
+#gunzip -k /scratch/general/nfs1/utu_4310/fr10_evolution_wd/reference_genomes/*.gz
 mkdir /scratch/general/nfs1/utu_4310/fr10_evolution_wd/fnas_dbs
-mv /scratch/general/nfs1/utu_4310/fr10_evolution_wd/reference_genomes/fna$ /scratch/general/nfs1/utu_4310/fr10_evolution_wd/fnas_dbs
+mv /scratch/general/nfs1/utu_4310/fr10_evolution_wd/reference_genomes/*.fna /scratch/general/nfs1/utu_4310/fr10_evolution_wd/fnas_dbs
 
 #make blast db for each genome
 
@@ -14,6 +15,6 @@ for genome in /scratch/general/nfs1/utu_4310/fr10_evolution_wd/fnas_dbs/*.fna; d
 done
 
 #blast each genome for fr10
-for genome in /scratch/general/nfs1/utu_4310/fr10_evolution_wd/fnas_dbs/*.fna; do blastn -query  /uufs/chpc.utah.edu/common/home/u6052680/fr10_evolution/fr10.fasta -db "$genome.db" -evalue 0.001 -outfmt "6 qseqid sseqid evalue sseq -out "$genome.db.results.txt"
+for genome in /scratch/general/nfs1/utu_4310/fr10_evolution_wd/fnas_dbs/*.fna; do blastn -query  /uufs/chpc.utah.edu/common/home/u6052680/fr10_evolution/fr10.fasta -db "$genome.db" -evalue 0.001 -outfmt "6 stitle qseqid sseqid evalue sseq" -out "$genome.db.results.txt"
 done
 mv /scratch/general/nfs1/utu_4310/fr10_evolution_wd/fnas_dbs/*.db.results.txt /scratch/general/nfs1/utu_4310/fr10_evolution_wd/blast_outputs

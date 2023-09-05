c9565041bc9bf8124a8bc29ed3a98391e568566f
KLab-UT <utt.klab@gmail.com>
2023-09-05 10:08:41 -0600

    created translate script for blast results

--- translate_blast_output.py
+++ translate_blast_output.py
@@ -10,23 +10,22 @@ def get_reading_frames(blast_line):
     seq1 = seq_hit
     seq1 = addN(seq1)
     print("seq1",seq1)
-    seq2 = seq_hit[1:]
-    seq2 = addN(seq2)
+    seq2 = (seq1[1:] + "N")
     print("seq2",seq2)
-    seq3 = seq_hit[3:]
-    seq3 = addN(seq3)
+    seq3 = (seq2[1:] + "N")
     print("seq3",seq3,"\n")
     seq_list = [seq1, seq2, seq3]
-    return str(seq_list)
+    return seq_list
 
 def addN(sequence):
     N_to_add = len(sequence) % 3
     sequence = sequence + 'N' * N_to_add
     return sequence
 
-def write_seqs_to_file(name, sequence, index, outfile):
-    outfile.write(">" + name + str(index) + "\n") 
-    outfile.write(str(Seq(sequence).translate()))
+def write_seqs_to_file(name, seq_list, outfile):
+    for index,sequence in enumerate(seq_list):
+        outfile.write(">" + name + str(index) + "\n") 
+        outfile.write(str(Seq(sequence).translate()) + "\n")
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as infile:
@@ -35,6 +34,5 @@ if __name__ == "__main__":
                 name = line.split("\t")[0]
                 print(name)
                 seq_list = get_reading_frames(line)
-                seq_list
-
+                write_seqs_to_file(name, seq_list, outfile)
 
